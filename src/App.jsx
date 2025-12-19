2df320369caa1eeeb8dbc4a3b160b093877cb766
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2025-12-19 17:10:21 +0530

    SideBar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Nav from "./components/Nav";
+import SideBar from "./SideBar";
 
 export default function App() {
   return (
     <div>
       <Nav/>
+      <SideBar/>
     </div>
   )
 }
---
+++ src/SideBar.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { GoHome } from "react-icons/go";
+import { SiYoutubeshorts } from "react-icons/si";
+import { MdOutlineSubscriptions } from "react-icons/md";
+import { MdDownload } from "react-icons/md";
+
+
+
+function SideBar() {
+  return (
+    <div className="px-5 w-[15%]">
+
+      
+   <div className=" space-y-3 items-center">
+
+    <div className="flex items-center space-x-6 cursor-pointer text-xl  hover:bg-gray-300 duration-300 rounded-xl p-1">
+     <GoHome/>
+     <span>Home</span>
+    </div>
+ 
+ {/* make a button of Shorts */}
+
+     <div className="flex items-center space-x-6 cursor-pointer text-xl  hover:bg-gray-300 duration-300 rounded-xl p-1">
+     <SiYoutubeshorts />
+
+     <span>Shorts</span>
+    </div>
+
+    {/* make a button of Subscritption */}
+     <div className="flex items-center space-x-6 cursor-pointer text-xl  hover:bg-gray-300 duration-300 rounded-xl p-1">
+   <MdOutlineSubscriptions />
+    <span>Subscriptions</span>
+    </div>
+    
+     {/* make a button of Download */}
+ <div className="flex items-center space-x-6 cursor-pointer text-xl  hover:bg-gray-300 duration-300 rounded-xl p-1">
+     <MdDownload />
+     <span>Download</span>
+    </div>
+
+   </div>
+
+ <hr/>
+    </div>
+    
+
+  )
+}
+
+export default SideBar
